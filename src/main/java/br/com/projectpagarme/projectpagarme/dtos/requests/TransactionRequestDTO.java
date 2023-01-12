51fc4f9132c30e18feb0c7341f4a6a2ad0e73fb0
caiqueFelix1 <122454806+caiqueFelix1@users.noreply.github.com>
2023-01-12 19:36:07 -0300

    :sparkle: feat: created TransactionRequestDTO
    uploading TransactionRequestDTO.java class

---
+++ src/main/java/br/com/projectpagarme/projectpagarme/dtos/requests/TransactionRequestDTO.java
@@ -0,0 +1,30 @@
+package br.com.projectpagarme.projectpagarme.dtos.requests;
+
+import com.fasterxml.jackson.annotation.JsonFormat;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.math.BigDecimal;
+import java.util.Date;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
+public class TransactionRequestDTO {
+
+    private BigDecimal transactionValue;
+
+    private String transactionDescription;
+
+    private String typePayment;
+
+    private String cardNumber;
+
+    private String cardOwnerName;
+
+    @JsonFormat(pattern = "yyyy/MM/dd")
+    private Date dateExpirationCard;
+
+    private String cvv;
+}
