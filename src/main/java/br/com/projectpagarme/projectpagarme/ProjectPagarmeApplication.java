bca05de61460efdedf16069068988d9874629dfc
caiqueFelix1 <122454806+caiqueFelix1@users.noreply.github.com>
2023-01-12 19:52:58 -0300

    :recycle: refactor: configured ProjectPagarmeApplication.java
    uploading new configuration in ProjectPagarmeApplication.java class

---
+++ src/main/java/br/com/projectpagarme/projectpagarme/ProjectPagarmeApplication.java
@@ -0,0 +1,23 @@
+package br.com.projectpagarme.projectpagarme;
+
+import org.modelmapper.ModelMapper;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
+import org.springframework.context.annotation.Bean;
+
+@SpringBootApplication (exclude = {SecurityAutoConfiguration.class})
+public class ProjectPagarmeApplication {
+
+	public static void main(String[] args) {
+
+		SpringApplication.run(ProjectPagarmeApplication.class, args);
+	}
+
+	@Bean
+	public ModelMapper modelMapper(){
+
+		return new ModelMapper();
+	}
+
+}
