839edd92df3007ddc59bbd6f5336e6540764f1f0
caiqueFelix1 <122454806+caiqueFelix1@users.noreply.github.com>
2023-01-12 20:17:34 -0300

    :sparkle: feat: created pagarme project
    uploading new springboot project

---
+++ src/test/java/br/com/projectpagarme/projectpagarme/ProjectPagarmeApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.projectpagarme.projectpagarme;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjectPagarmeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
