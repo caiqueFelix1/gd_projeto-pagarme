cbfa5c930f945c0f7a7ebe6e898ccb8a2971b0f8
caiqueFelix1 <122454806+caiqueFelix1@users.noreply.github.com>
2023-01-12 19:36:40 -0300

    :sparkle: feat:
    uploading PaymentResponseDTO.java class

---
+++ src/main/java/br/com/projectpagarme/projectpagarme/dtos/responses/PaymentResponseDTO.java
@@ -0,0 +1,17 @@
+package br.com.projectpagarme.projectpagarme.dtos.responses;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDate;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
+public class PaymentResponseDTO {
+
+    private String status;
+
+    private LocalDate paymentDate;
+}
