4fbb1b9f3835d3ff757a9158dcb2de46b410b6dc
caiqueFelix1 <122454806+caiqueFelix1@users.noreply.github.com>
2023-01-16 15:37:59 -0300

    :sparkle: feat:
    created a new class, ApplicationExceptionHandler

---
+++ src/main/java/br/com/projectpagarme/projectpagarme/exceptions/ApplicationExceptionHandler.java
@@ -0,0 +1,45 @@
+package br.com.projectpagarme.projectpagarme.exceptions;
+
+
+import br.com.projectpagarme.projectpagarme.exceptions.models.ErrorObject;
+import br.com.projectpagarme.projectpagarme.exceptions.models.ErrorResponse;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.context.request.WebRequest;
+import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
+import java.time.Instant;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Collectors;
+@ControllerAdvice
+@Slf4j
+public class ApplicationExceptionHandler extends ResponseEntityExceptionHandler {
+
+    @Override
+    protected ResponseEntity<Object> handleMethodArgumentNotValid(MethodArgumentNotValidException ex, HttpHeaders headers, HttpStatus status, WebRequest request) {
+        List<ErrorObject> errors = getErrors(ex);
+        ErrorResponse errorResponse = getErrorResponse(status, errors);
+        log.error("erro de entrada de dados: {}", errorResponse);
+        return new ResponseEntity<>(errorResponse, status);
+    }
+
+    private ErrorResponse getErrorResponse(HttpStatus status, List<ErrorObject> errors) {
+        return new ErrorResponse(Instant.now().toEpochMilli(),
+                status.value(),
+                status.getReasonPhrase(),
+                "Requisição possui campos inválidos",
+                errors);
+    }
+
+    private List<ErrorObject> getErrors(MethodArgumentNotValidException ex) {
+        return ex.getBindingResult().getFieldErrors().stream()
+                .map(error -> new ErrorObject(error.getDefaultMessage(), error.getField(), error.getRejectedValue()))
+                .collect(Collectors.toList());
+    }
+}
