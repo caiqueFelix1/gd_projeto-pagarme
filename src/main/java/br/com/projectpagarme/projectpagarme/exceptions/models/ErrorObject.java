164a0eb7ac20050333e1f49357497944f39a80a9
caiqueFelix1 <122454806+caiqueFelix1@users.noreply.github.com>
2023-01-16 15:38:38 -0300

    :sparkle: feat:
    created a new class, ErrorObject

---
+++ src/main/java/br/com/projectpagarme/projectpagarme/exceptions/models/ErrorObject.java
@@ -0,0 +1,15 @@
+package br.com.projectpagarme.projectpagarme.exceptions.models;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+@AllArgsConstructor
+@Data
+public class ErrorObject{
+
+    private final String message;
+
+    private final String field;
+
+    private final Object parameter;
+}
