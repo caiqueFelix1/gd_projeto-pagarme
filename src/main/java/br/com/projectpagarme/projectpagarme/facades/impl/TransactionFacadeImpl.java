32164215a88983aceaf49e8150af1940ec884990
caiqueFelix1 <122454806+caiqueFelix1@users.noreply.github.com>
2023-01-16 15:49:43 -0300

    :recycle: refactor:
    created a new solution to sum all balance

--- src/main/java/br/com/projectpagarme/projectpagarme/facades/impl/TransactionFacadeImpl.java
+++ src/main/java/br/com/projectpagarme/projectpagarme/facades/impl/TransactionFacadeImpl.java
@@ -1,13 +1,19 @@
 package br.com.projectpagarme.projectpagarme.facades.impl;
 
 import br.com.projectpagarme.projectpagarme.dtos.requests.TransactionRequestDTO;
+import br.com.projectpagarme.projectpagarme.dtos.responses.BalanceResponseDTO;
 import br.com.projectpagarme.projectpagarme.dtos.responses.TransactionResponseDTO;
+import br.com.projectpagarme.projectpagarme.entities.TransactionEntity;
+import br.com.projectpagarme.projectpagarme.enums.PaymentMapperEnum;
 import br.com.projectpagarme.projectpagarme.facades.TransactionFacade;
 import br.com.projectpagarme.projectpagarme.mapper.TransactionMapper;
 import br.com.projectpagarme.projectpagarme.services.TransactionService;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
+
+import java.math.BigDecimal;
+import java.util.List;
 
 @Slf4j
 @Component
@@ -30,4 +36,33 @@ public class TransactionFacadeImpl implements TransactionFacade {
         return transactionMapper.toDto(transactionService.create(transactionMapper.toEntity(transactionRequestDTO)));
     }
 
+    public BalanceResponseDTO getBalance() {
+
+        BalanceResponseDTO balanceResponseDTO = new BalanceResponseDTO();
+
+        for (TransactionEntity transactionEntity : transactionService.getAll()) {
+
+            if (transactionEntity.getTypePayment() == PaymentMapperEnum.CREDIT_CARD) {
+                BigDecimal waiting_funds = transactionEntity.getTransactionValue();
+                waiting_funds = waiting_funds.add(waiting_funds);
+
+                balanceResponseDTO.setWaiting_funds(waiting_funds);
+            }
+
+            if (transactionEntity.getTypePayment() == PaymentMapperEnum.DEBIT_CARD){
+                BigDecimal avaible = transactionEntity.getTransactionValue();
+                avaible = avaible.add(avaible);
+
+                balanceResponseDTO.setAvailable(avaible);
+            }
+        }
+
+        return balanceResponseDTO;
+    }
+
+    @Override
+    public List<TransactionResponseDTO> getAll() {
+
+        return transactionMapper.toListDto(transactionService.getAll());
+    }
 }
