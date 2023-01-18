08f6c0a8479d6f7588040ce54ebcd268fcc487df
caiqueFelix1 <122454806+caiqueFelix1@users.noreply.github.com>
2023-01-17 22:44:26 -0300

    :recycle: refactor:
    fixed method that calc to debit and credit fess, modify in if in transactionfacade

--- src/main/java/br/com/projectpagarme/projectpagarme/facades/impl/TransactionFacadeImpl.java
+++ src/main/java/br/com/projectpagarme/projectpagarme/facades/impl/TransactionFacadeImpl.java
@@ -6,7 +6,7 @@ import br.com.projectpagarme.projectpagarme.dtos.responses.TransactionResponseDT
 import br.com.projectpagarme.projectpagarme.entities.TransactionEntity;
 import br.com.projectpagarme.projectpagarme.enums.PaymentMapperEnum;
 import br.com.projectpagarme.projectpagarme.facades.TransactionFacade;
-import br.com.projectpagarme.projectpagarme.mapper.TransactionMapper;
+import br.com.projectpagarme.projectpagarme.mappers.TransactionMapper;
 import br.com.projectpagarme.projectpagarme.services.TransactionService;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -28,34 +28,33 @@ public class TransactionFacadeImpl implements TransactionFacade {
     @Override
     public TransactionResponseDTO create(TransactionRequestDTO transactionRequestDTO) {
 
-        String getCardNumber = transactionRequestDTO.getCardNumber();
-        String cardNumberNotSalved = getCardNumber.substring(getCardNumber.length()-4);
-
-        transactionRequestDTO.setCardNumber(cardNumberNotSalved);
+        transactionRequestDTO.setCardNumber(hidingCardDigits(transactionRequestDTO.getCardNumber()));
 
         return transactionMapper.toDto(transactionService.create(transactionMapper.toEntity(transactionRequestDTO)));
     }
 
-    public BalanceResponseDTO getBalance() {
+    public BalanceResponseDTO getBalance(BigDecimal valueTotal) {
 
-        BalanceResponseDTO balanceResponseDTO = new BalanceResponseDTO();
+        BigDecimal waiting_funds = BigDecimal.ZERO;
+
+        BigDecimal available = BigDecimal.ZERO;
 
         for (TransactionEntity transactionEntity : transactionService.getAll()) {
 
             if (transactionEntity.getTypePayment() == PaymentMapperEnum.CREDIT_CARD) {
-                BigDecimal waiting_funds = transactionEntity.getTransactionValue();
-                waiting_funds = waiting_funds.add(waiting_funds);
-
-                balanceResponseDTO.setWaiting_funds(waiting_funds);
+                BigDecimal waiting = transactionEntity.getTransactionValue();
+                waiting_funds = waiting_funds.add(waiting);
             }
 
             if (transactionEntity.getTypePayment() == PaymentMapperEnum.DEBIT_CARD){
-                BigDecimal avaible = transactionEntity.getTransactionValue();
-                avaible = avaible.add(avaible);
-
-                balanceResponseDTO.setAvailable(avaible);
+                BigDecimal accessible = transactionEntity.getTransactionValue();
+                available = available.add(accessible);
             }
         }
+
+        BalanceResponseDTO balanceResponseDTO = new BalanceResponseDTO();
+        balanceResponseDTO.setAvailable(available);
+        balanceResponseDTO.setWaiting_funds(waiting_funds);
 
         return balanceResponseDTO;
     }
@@ -64,5 +63,13 @@ public class TransactionFacadeImpl implements TransactionFacade {
     public List<TransactionResponseDTO> getAll() {
 
         return transactionMapper.toListDto(transactionService.getAll());
+    }
+
+    private String hidingCardDigits(String numberCard){
+
+        final String cardDigits = "**** **** **** ";
+        String lastFourNumberCard = numberCard.substring(numberCard.length()-4);
+
+        return cardDigits + lastFourNumberCard;
     }
 }
