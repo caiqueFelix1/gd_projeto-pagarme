2aa16b1f4baf539e52511d3e2af1d88e362942a4
caiqueFelix1 <122454806+caiqueFelix1@users.noreply.github.com>
2023-01-17 22:36:59 -0300

    :recycle: refactor:
    changed variable type, type payment to enum, date expiration card to string and removed cvv of response in Transction Response Dto

--- src/main/java/br/com/projectpagarme/projectpagarme/dtos/responses/TransactionResponseDTO.java
+++ src/main/java/br/com/projectpagarme/projectpagarme/dtos/responses/TransactionResponseDTO.java
@@ -1,11 +1,13 @@
 package br.com.projectpagarme.projectpagarme.dtos.responses;
 
+import br.com.projectpagarme.projectpagarme.enums.PaymentMapperEnum;
 import com.fasterxml.jackson.annotation.JsonFormat;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
 import java.math.BigDecimal;
+import java.time.LocalDate;
 import java.util.Date;
 
 @AllArgsConstructor
@@ -13,20 +15,19 @@ import java.util.Date;
 @Data
 public class TransactionResponseDTO {
 
+    private Long id;
+
     private BigDecimal transactionValue;
 
     private String transactionDescription;
 
-    private String typePayment;
+    private PaymentMapperEnum typePayment;
 
     private String cardNumber;
 
     private String cardOwnerName;
 
-    @JsonFormat(pattern = "yyyy/MM/dd")
-    private Date dateExpirationCard;
-
-    private String cvv;
+    private String dateExpirationCard;
 
     private PaymentResponseDTO payment;
 }
