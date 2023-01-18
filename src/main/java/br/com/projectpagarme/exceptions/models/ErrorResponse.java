595e517dbcd6998e09b542c187be1fe48d4e5353
caiqueFelix1 <122454806+caiqueFelix1@users.noreply.github.com>
2023-01-18 01:02:30 -0300

    :sparkles: feat: created ErrorResponse.java
    The java recreated ErrorResponse.java after I changed the projcet name.

---
+++ src/main/java/br/com/projectpagarme/exceptions/models/ErrorResponse.java
@@ -0,0 +1,25 @@
+package br.com.projectpagarme.exceptions.models;
+
+import com.fasterxml.jackson.annotation.JsonInclude;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.List;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
+public class ErrorResponse {
+
+    private Long timestamp;
+
+    private Integer status;
+
+    private String code;
+
+    private String message;
+
+    @JsonInclude(JsonInclude.Include.NON_EMPTY)
+    private List<ErrorObject> errors;
+}
